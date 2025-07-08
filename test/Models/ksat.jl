d1d2182bcee63807bdba4de7d128a25420ddd4df
stecrotti <sky_96@live.it>
2025-07-08 22:43:27 +0200

    test MCMC for ksat

--- test/Models/ksat.jl
+++ test/Models/ksat.jl
@@ -1,20 +1,3 @@
-@testset "ksat tree" begin
-    A = [1 0 0 0 1; 0 1 0 0 0; 1 1 1 1 0]
-    g = FactorGraph(A)
-    factors = [KSATClause{BitVector}(Bool[0, 1]), KSATClause{BitVector}(Bool[1]), KSATClause{BitVector}(Bool[0, 0, 1, 1])]
-    variable_biases = fill(TabulatedFactor([0.6, 0.4]), nvariables(g))
-    model = MarkovRandomField(g, factors, fill(2, eachvariable(g)); variable_biases)
-    p_ex = exact_prob(model)
-
-    # test that probability of an unsat configuration is zero
-    @test all(zip(eachstate(model), p_ex)) do (x, p)
-        nunsat = sum(1 - Int(weight(model.factors[a], x[i] 
-            for i in neighbors(model.graph, f_vertex(a)))) 
-            for a in eachfactor(model.graph))
-        (nunsat == 0) ⊻ (p == 0)
-    end
-end
-
 @testset "ksat random graph" begin
     n = 10
     m = 3
@@ -32,4 +15,21 @@ end
             for a in eachfactor(model.graph))
         (nunsat == 0) ⊻ (p == 0)
     end
+
+    @testset "Metropolis ksat" begin
+        samples = sample_mh(model; nsamples=10^4)
+        m = mean(samples[end÷4:end])
+        marg = exact_marginals(model)
+        m_ex = [sum(eachindex(margi).*margi) for margi in marg]
+        @test all(abs.(m .- m_ex) .< 1e-1)
+    end
+
+    @testset "Gibbs ksat" begin
+        samples = sample_gibbs(model; nsamples=10^6)
+        m = mean(samples[end÷4:end])
+        marg = exact_marginals(model)
+        m_ex = [sum(eachindex(margi).*margi) for margi in marg]
+        @test all(abs.(m .- m_ex) .< 1e-2)
+    end
 end
+
